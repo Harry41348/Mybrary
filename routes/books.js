475380990cd04cf7ef35e70997659d2ace9eb392
Harry41348 <hvalentinow@gmail.com>
2022-05-18 20:02:43 +0100

    Fixed book creation error

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,10 @@
 // Imports
 const express = require('express');
-const res = require('express/lib/response');
 const router = express.Router();
 const Author = require('../models/author');
 const Book = require('../models/book');
+
+const imageMimeTypes = ['image/jpeg', 'image/png'];
 
 // All books route
 router.get('/', async (req, res) => {
